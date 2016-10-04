a853a853113922eac83c9bc11fa4b15e88754412
Hreiðar Ólafur Arnarsson <spekingurinn@gmail.com>
2016-10-04 22:00:22 +0000

    Update T6.sql
    Added SQL code for Task 6

--- Project3/Tasks/T6.sql
+++ Project3/Tasks/T6.sql
@@ -1,1 +1,20 @@
+/* Task 6 
+ * Trigger NewPerson should create a new account when a new person is inserted into People.
+ * Each new customer gets an initial overdraft of 10,000 (ten thousand).
+ */
+create or replace function NewPerson()
+returns trigger
+as
+$$
+begin
+	insert into Accounts (PID, aDate, aBalance, aOver)		-- nota serial fyrir AID?
+	values (NEW.PID, current_date, 0, 10000);			-- adds values into table
+	return NEW;
+end;
+$$
+language 'plpgsql';
 
+create trigger NewPerson
+after insert on People
+for each row 
+execute procedure NewPerson();
