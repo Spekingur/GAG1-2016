38cc3b40c7c4c758ee3b061451cde0703273be5e
Maciuska <Maciuska14@gmail.com>
2016-10-05 21:30:33 +0000

    Update T2.sql

--- Project3/Tasks/T2.sql
+++ Project3/Tasks/T2.sql
@@ -1,1 +1,9 @@
-
+CREATE VIEW DebtorStatus
+AS
+SELECT A.pid, P.pname, SUM(A.abalance) AS abalance, SUM(A.aover) AS aover
+FROM accounts A
+JOIN people P
+ON A.pid = P.pid
+WHERE A.abalance < 0
+GROUP BY A.pid, P.pname
+ORDER BY A.pid
