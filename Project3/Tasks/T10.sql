afa2cad5078672ba7a105bf10385a6875e282615
Hreiðar Ólafur Arnarsson <spekingurinn@gmail.com>
2016-10-05 14:37:17 +0000

    Update T10.sql
    Added checks on iAmount to make sure it is a positive amount greater than zero.
    Also added description of these checks.

--- Project3/Tasks/T10.sql
+++ Project3/Tasks/T10.sql
@@ -14,13 +14,24 @@ $$
 declare
 	billPID int;
 begin
-	insert into AccountRecords (AID, rDate, rType, rAmount)		-- no insert of balance since we assume that trigger from #5 will add it up for us
-	values (iAID, current_date, 'L', iAmount);
-
-	billPID := (select PID from Accounts where AID = iAID);		-- getting the PID for the account in question so we know who get the bill
+	/* We check if iAmount is greater than zero since it does not make sense to 
+	 * loan an amount of money of zero or less 
+	 */
+	if iAmount = 0 then
+		raise exception 'Can not loan zero amount of money!';
 	
-	insert into Bills (PID, bDueDate, bAmount, bIsPaid)		-- trigger from #4 should check if this insert is okay
-	values (billPID, iDueDate, iAmount, false);
+	elsif iAmount < 0 then
+		raise exception 'Can not loan negative amount of money!';
+	
+	elsif iAmount > 0 then
+		insert into AccountRecords (AID, rDate, rType, rAmount)		-- no insert of balance since we assume that trigger from #5 will add it up for us
+		values (iAID, current_date, 'L', iAmount);
+
+		billPID := (select PID from Accounts where AID = iAID);		-- getting the PID for the account in question so we know who get the bill
+	
+		insert into Bills (PID, bDueDate, bAmount, bIsPaid)		-- trigger from #4 should check if this insert is okay
+		values (billPID, iDueDate, iAmount, false);
+	end if;
 end;
 $$
 language 'plpgsql';
