538caabcbc04753563768f0bccb61f243dfbbb57
Hreiðar Ólafur Arnarsson <spekingurinn@gmail.com>
2016-10-05 14:46:51 +0000

    Update CODE.sql
    Added placeholder text

--- Project3/CODE.sql
+++ Project3/CODE.sql
@@ -1,1 +1,24 @@
+/************** Project 3 *********************
+ * Hreiðar Ólafur Arnarsson - hreidara14@ru.is 
+ * Maciej Sierzputowski - maciej15@ru.is
+ **********************************************/
+ 
+/* Task 1 */
 
+/* Task 2 */
+
+/* Task 3 */
+
+/* Task 4 */
+
+/* Task 5 */
+
+/* Task 6 */
+
+/* Task 7 */
+
+/* Task 8 */
+
+/* Task 9 */
+
+/* Task 10 */
