4aa3f391bd23532f08406dfb45e2bccfb8cf1c1d
Maciuska <Maciuska14@gmail.com>
2016-10-05 21:30:13 +0000

    Update T1.sql

--- Project3/Tasks/T1.sql
+++ Project3/Tasks/T1.sql
@@ -1,1 +1,6 @@
-
+CREATE VIEW AllAccountRecords
+AS
+SELECT A.aid, A.pid, A.adate, A.abalance, A.aover, R.rid, R.rdate, R.rtype, R.ramount, R.rbalance
+FROM accounts A
+LEFT JOIN accountrecords R
+ON A.aid = R.aid
