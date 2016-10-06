4693d00c2452d0a7e31e3ca9ce0ad1387ec70a18
Hreiðar Ólafur Arnarsson <spekingurinn@gmail.com>
2016-10-06 14:02:04 +0000

    Update CODE.sql
    Updated with Task #7, #9 and #10 (functions)

--- Project3/CODE.sql
+++ Project3/CODE.sql
@@ -137,9 +137,85 @@ for each row
 execute procedure NewPerson();
 
 /* Task 7 */
+create or replace function InsertPerson (
+	IN iName varchar(50), 
+	IN iGender char(1), 
+	IN iHeight float, 
+	IN iAmount int)
+returns void
+as
+$$
+declare
+	nAID int;
+begin
+	insert into People (pName, pGender, pHeight)		-- nota serial fyrir PID til að halda númeraröð ef það skyldi koma upp villa?
+	values (iName, iGender, iHeight);			-- adding input values into People table, should start trigger from #6
+
+	nAID := lastval();					-- get the value of unique ID from trigger
+
+	insert into AccountRecords (AID, rDate, rType, rAmount)	-- balance is not specified because trigger from #5 should take care of adding things up
+	values (nAID, current_date, 'O', iAmount);		-- uses type (O)ther because this is not a (B)ill, (T)ransfer or (L)oan
+end;
+$$
+language 'plpgsql';
 
 /* Task 8 */
 
 /* Task 9 */
+create or replace function Transfer (
+	IN iToAID int,
+	IN iFromAID int,
+	IN iAmount int)
+returns void
+as
+$$
+begin
+	if iAmount = 0 then		-- making sure you don't transfer zero money
+		raise exception 'Can not transfer no money';
+	elsif iAmount < 0 then		-- making sure you don't transfer minus money (a reverse transfer)
+		raise exception 'Can not transfer minus money';
+	elsif iAmount > 0 then
+		/* Should be no need to make other checks since trigger from #5 should take care of checking if
+		   iAmount is more than exists on given iFromAID account. */
+		insert into AccountRecords (AID, rDate, rType, rAmount)		-- We don't insert balance because trigger #5 finds the balance and inserts it
+		values 
+		(iFromAID, current_date, 'T', iAmount),			-- iFromAID, transfer iAmount from account
+		(iToAID, current_date, 'T', iAmount);			-- iToAID, transfer iAmount to account
+	end if;
+end;
+$$
+language 'plpgsql';
 
 /* Task 10 */
+create or replace function LoanMoney (
+	IN iAID int,
+	IN iAmount int,
+	IN iDueDate date)
+returns void
+as
+$$
+declare
+	billPID int;
+begin
+	/*************************************************************************** 
+	 * We check if iAmount is greater than zero since it does not make sense to 
+	 * loan an amount of money of zero or less 
+	 ***************************************************************************/
+	if iAmount = 0 then
+		raise exception 'Can not loan zero amount of money!';
+	
+	elsif iAmount < 0 then
+		raise exception 'Can not loan negative amount of money!';
+	
+	elsif iAmount > 0 then
+		insert into AccountRecords (AID, rDate, rType, rAmount)		-- no insert of balance since we assume that trigger from #5 will add it up for us
+		values (iAID, current_date, 'L', iAmount);
+
+		billPID := (select PID from Accounts where AID = iAID);		-- getting the PID for the account in question so we know who get the bill
+	
+		insert into Bills (PID, bDueDate, bAmount, bIsPaid)		-- trigger from #4 should check if this insert is okay
+		values (billPID, iDueDate, iAmount, false);
+	end if;
+end;
+$$
+language 'plpgsql';
