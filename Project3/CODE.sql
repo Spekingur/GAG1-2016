a02bc532739b2f8ab409a37dc097ff8ceaf4c605
Hreiðar Ólafur Arnarsson <spekingurinn@gmail.com>
2016-10-06 22:29:04 +0000

    Update CODE.sql
    Fixed minus sign in iFromAID action for Task #9

--- Project3/CODE.sql
+++ Project3/CODE.sql
@@ -179,7 +179,7 @@ begin
 		   iAmount is more than exists on given iFromAID account. */
 		insert into AccountRecords (AID, rDate, rType, rAmount)		-- We don't insert balance because trigger #5 finds the balance and inserts it
 		values 
-		(iFromAID, current_date, 'T', iAmount),			-- iFromAID, transfer iAmount from account
+		(iFromAID, current_date, 'T', -iAmount),			-- iFromAID, transfer iAmount from account
 		(iToAID, current_date, 'T', iAmount);			-- iToAID, transfer iAmount to account
 	end if;
 end;
